08fadbbdc734883e5ca78ed298faa5fa33f6d69e
smickel20 <75023071+smickel20@users.noreply.github.com>
2025-03-18 12:31:41 +0100

    added controllers

---
+++ LU1-1.3/Controllers/ChildController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LU1_1._3.Controllers
+{
+    public class ChildController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
