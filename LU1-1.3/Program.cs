8c8b590629c2ec044c25f75892cf2bdddc6271be
smickel20 <75023071+smickel20@users.noreply.github.com>
2025-03-18 11:52:39 +0100

    init program.cs

--- LU1-1.3/Program.cs
+++ LU1-1.3/Program.cs
@@ -1,23 +1,55 @@
+using System.Diagnostics;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddAuthorization();
 
+builder.Configuration.AddUserSecrets<Program>();
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("UserOwnsResource", policy =>
+        policy.RequireAssertion(context =>
+        {
+            var userId = context.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var resourceUserId = context.Resource as string; // Assuming resourceUserId is passed as a string
+
+            return userId == resourceUserId;
+        }));
+});
+
+string connStr = builder.Configuration.GetValue<string>("ConnectionString");
+builder.Services.AddIdentityApiEndpoints<IdentityUser>(options =>
+{
+    options.User.RequireUniqueEmail = true;
+    options.Password.RequiredLength = 10;
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+
+}).AddRoles<IdentityRole>()
+    .AddDapperStores(options =>
+    {
+        options.ConnectionString = connStr;
+    });
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddLogging();
 
+var sqlConnectionStringFound = !string.IsNullOrWhiteSpace(connStr);
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
+app.MapGet("/", () => $"The API is up. Connection string found: {(sqlConnectionStringFound ? "Yes" : "No")}");
 app.MapControllers();
-
+app.MapGroup("/account").MapIdentityApi<IdentityUser>();
+app.UseHttpsRedirection();
+app.UseAuthorization();
 app.Run();
